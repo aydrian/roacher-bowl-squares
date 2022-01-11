dc4325d695287c6868cad8f3a074bd96dde80991
Aydrian Howard <aydrian@gmail.com>
2022-01-11 16:55:07 -0500

    initial commit

---
+++ scripts/init_database.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS roacher_bowl_squares;
+
+USE roacher_bowl_squares;
+
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMPTZ DEFAULT now(),
+  updated_at TIMESTAMPTZ DEFAULT now(),
+  username STRING UNIQUE NOT NULL,
+  password_hash STRING NOT NULL
+);
+
+CREATE TABLE games (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  slug STRING UNIQUE NOT NULL,
+  host_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  state STRING NOT NULL DEFAULT 'INIT' CHECK (state = 'INIT' OR state = 'Q1' OR state = 'Q2' OR state = 'Q3' OR state = 'Q4' OR state = 'FINAL'),
+  claim_cost DECIMAL NOT NULL DEFAULT 1.0,
+  board JSONB DEFAULT '{}',
+  scores JSONB DEFAULT '[]',
+  winners JSON DEFAULT '[]',
+  created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE claims (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  game_id UUID NOT NULL REFERENCES games (id) ON DELETE CASCADE,
+  participant_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  row INT NOT NULL,
+  col INT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT now()
+);
