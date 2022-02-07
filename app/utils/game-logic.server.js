cf9c9a4fa97c394bf2e1ef565c727915d1680aa2
Aydrian Howard <aydrian@gmail.com>
2022-02-07 10:22:09 -0500

    saving userid in winners

--- app/utils/game-logic.server.js
+++ app/utils/game-logic.server.js
@@ -150,7 +150,8 @@ function updateWinners(winners, quarter, winningClaim) {
   let [q1Winner, q2Winner, q3Winner, q4Winner] = winners;
   const winner = {
     id: winningClaim.participant.id,
-    name: winningClaim.participant.username
+    name: winningClaim.participant.username,
+    userid: winningClaim.participant.userid
   };
   switch (quarter) {
     case "Q1":
@@ -166,6 +167,7 @@ function updateWinners(winners, quarter, winningClaim) {
       q4Winner = winner;
       break;
   }
+  console.log([q1Winner, q2Winner, q3Winner, q4Winner]);
   return [q1Winner, q2Winner, q3Winner, q4Winner].filter(
     (val) => val !== undefined
   );
