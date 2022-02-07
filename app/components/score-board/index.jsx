a2a0ec62f8da67c530e7109050214fb0faa942aa
Aydrian Howard <aydrian@gmail.com>
2022-02-06 21:06:45 -0500

    good start on the new scoreboard

--- app/components/score-board/index.jsx
+++ app/components/score-board/index.jsx
@@ -1,21 +1,81 @@
+import { useState } from "react";
 import { Form } from "remix";
 import styles from "./styles.css";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
+import dialogStyleUrl from "@reach/dialog/styles.css";
 
-export const links = () => [{ rel: "stylesheet", href: styles }];
+export const links = () => [
+  { rel: "stylesheet", href: dialogStyleUrl },
+  { rel: "stylesheet", href: styles }
+];
 
 export function ScoreBoard({ game, isHost }) {
+  // TODO: Make dialog close on submit
+  const [showDialog, setShowDialog] = useState(false);
   const { board, state, scores } = game;
   const [team1 = "Team 1", team2 = "Team 2"] = board.teams;
-  // scores = [[0,0], [0,0], [0,0], [0,0]]
   const quarters = [
-    ["Q1", "1st Quarter"],
-    ["Q2", "2nd Quarter", ["Q2", "Q3", "Q4", "FINAL"]],
-    ["Q3", "3rd Quarter", ["Q3", "Q4", "FINAL"]],
-    ["Q4", "4th Quarter", ["Q4", "FINAL"]]
+    ["Q1"],
+    ["Q2", ["Q2", "Q3", "Q4", "FINAL"]],
+    ["Q3", ["Q3", "Q4", "FINAL"]],
+    ["Q4", ["Q4", "FINAL"]]
   ];
 
   return (
     <div className="score-board-wrapper">
+      <div className="score-board-list">
+        {quarters.map(([quarter, showFor], i) => {
+          return (
+            <QuarterScores
+              key={quarter}
+              state={state}
+              quarter={quarter}
+              scores={scores[i]}
+              showFor={showFor}
+            />
+          );
+        })}
+      </div>
+      {isHost && state !== "FINAL" ? (
+        <>
+          <button
+            type="button"
+            className="button primary"
+            onClick={() => setShowDialog(true)}
+          >
+            Update Score
+          </button>
+          <DialogOverlay
+            isOpen={showDialog}
+            onDismiss={() => setShowDialog(false)}
+            style={{ backgroundColor: "hsla(0, 0%, 10%, 0.96)" }}
+          >
+            <DialogContent
+              aria-label="Join a game"
+              style={{ borderRadius: "15px" }}
+            >
+              <EditScoreBoard game={game} isHost={isHost} />
+            </DialogContent>
+          </DialogOverlay>
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+export function EditScoreBoard({ game, isHost }) {
+  const { board, state, scores } = game;
+  const [team1 = "Team 1", team2 = "Team 2"] = board.teams;
+  // scores = [[0,0], [0,0], [0,0], [0,0]]
+  const quarters = [
+    ["Q1", "Q1"],
+    ["Q2", "Q2", ["Q2", "Q3", "Q4", "FINAL"]],
+    ["Q3", "Q3", ["Q3", "Q4", "FINAL"]],
+    ["Q4", "Q4", ["Q4", "FINAL"]]
+  ];
+
+  return (
+    <div>
       <Form method="post" replace>
         <table className="score-board">
           <thead>
@@ -85,4 +145,20 @@ function ScoreRow({
       </td>
     </tr>
   ) : null;
+}
+
+function QuarterScores({
+  state,
+  quarter,
+  scores,
+  showFor = ["Q1", "Q2", "Q3", "Q4", "FINAL"]
+}) {
+  // TODO: dim quarters not in showFor
+  return (
+    <div className="quarter-scores">
+      <div className="quarter-title">{quarter}</div>
+      <div className="quarter-score team1">{scores && scores[0]}</div>
+      <div className="quarter-score team2">{scores && scores[1]}</div>
+    </div>
+  );
 }
