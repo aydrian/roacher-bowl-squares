6ce711a6bf6eed167d4126b8feceb388f1a012e2
Aydrian Howard <aydrian@gmail.com>
2022-02-08 09:27:34 -0500

    game-list updates

--- app/components/game-list/index.jsx
+++ app/components/game-list/index.jsx
@@ -4,12 +4,26 @@ import styles from "./styles.css";
 export const links = () => [{ rel: "stylesheet", href: styles }];
 
 function GameItem({ game }) {
+  const [team1, team2] = game.board.teams;
+  const stateTitles = {
+    INIT: "Claiming Squares",
+    Q1: "1st quarter",
+    Q2: "2nd quarter",
+    Q3: "3rd quarter",
+    Q4: "4th quarter",
+    FINAL: "Game Over"
+  };
   return (
-    <li className="game-item">
-      <Link to={`/games/${game.id}`}>
-        {game.slug} [{game.state}]
+    <li>
+      <Link to={`/games/${game.id}`} className="game-item">
+        <div className="game-item-title">
+          <div className="game-name">{game.slug}</div>
+          <div className="game-details">
+            {`${team1} vs ${team2}:`} {stateTitles[game.state]}
+          </div>
+        </div>
+        <img src="arrow.svg" width={15} />
       </Link>
-      <img src="arrow.svg" width={15} />
     </li>
   );
 }
